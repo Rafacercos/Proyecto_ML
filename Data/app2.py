fa8d3f8597c830c3fbadbeb704129c339449a954
rafacercos <rafacercosmatas@gmail.com>
2025-05-08 17:45:31 +0200

    mas

--- Data/app2.py
+++ Data/app2.py
@@ -3,16 +3,26 @@ import pandas as pd
 import numpy as np
 import joblib
 from PIL import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 model = joblib.load("modelo_viernes.pkl")
-
-st.set_page_config(page_title="Predicción de Riesgo Fiscal", layout="wide")
+st.set_page_config(page_title="FiscoGuard", layout="wide")
+st.title("FiscoGuard")
 
 imagen_url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Seal_of_the_United_States_Internal_Revenue_Service.svg/1200px-Seal_of_the_United_States_Internal_Revenue_Service.svg.png"
+
 
 st.markdown(
     f"""
     <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+    
+    html, body, [class*="st-"], h1, h2, h3, h4, h5, h6 {{
+        font-family: 'Roboto', sans-serif !important;
+    }}
+
     .stApp {{
         background-image: linear-gradient(rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.9)), 
                           url("{imagen_url}");
@@ -26,13 +36,13 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-logo = Image.open("lores_IRS.jpg")
+logo = Image.open("LogoML.pgn")
 st.image(logo, width=150)
 
 menu = st.sidebar.radio("Navegación", ["Inicio", "Información relevante", "Ayuda"])
 
 if menu == "Inicio":
-    st.title(" Herramienta de Predicción de Ingreso - Análisis de Riesgo Fiscal")
+    st.subheader("Herramienta de Predicción de Ingreso - Análisis de Riesgo Fiscal")
     st.markdown("---")
     st.markdown("Ingrese los datos del individuo para predecir si su ingreso anual es superior o inferior a 50.000 dólares.")
 
@@ -111,68 +121,93 @@ if menu == "Inicio":
         "marital-status_Nunca casado/a": [marital_nunca]
     })
 
-    
     if st.button("Predecir ingreso"):
+        
         pred = model.predict(input_data)[0]
         probas = model.predict_proba(input_data)[0]
         resultado = ">50K" if pred == 1 else "<=50K"
-
-        st.success(f" Predicción: El ingreso estimado es {resultado} dólares anuales")
+        
+        
+        st.success(f"Predicción: El ingreso estimado es {resultado} dólares anuales")
         st.markdown("**Probabilidades:**")
         st.markdown(f"- Probabilidad de ingreso <= 50K: {probas[0]*100:.2f}%")
         st.markdown(f"- Probabilidad de ingreso > 50K: {probas[1]*100:.2f}%")
 
-    with st.expander("Recordatorio: Tramos y Porcentajes de Impuestos en EE.UU."):
+        
+        st.subheader("Importancia de las características")
+        feature_names = input_data.columns[1:10]
+        feature_values = input_data.iloc[0].values[1:10]
+        fig, ax = plt.subplots(figsize=(8, 4))
+        ax.barh(["Trabajo","Estado Civil","Nivel Académico","Educación Superior","Horas Laborales (Semana)","Rango de Edad","Edad","Sexo"], feature_values[1:10])
+        ax.set_xlabel("Valor de la característica")
+        ax.set_title("Importancia de las características en la predicción")
+        st.pyplot(fig)
+    
+        with st.expander("Descripción variables"):
             st.markdown("""
-            **Tramos federales de impuestos sobre la renta para personas individuales (2024):**
-
-            - Hasta $11,000 → 10%
-            - Rango (11,001 - $44,725) → 12%
-            - Rango (44,726 - $95,375) → 22%
-            - Rango (95,376 - $182,100) → 24%
-            - Rango (182,101 - $231,250) → 32%
-            - Rango (231,251 - $578,125) → 35%
-            - Más de $578,125 → 37%
-
-            Los impuestos en EE.UU. son progresivos. Es decir, cada tramo se grava con su tipo impositivo específico.
-            [Fuente oficial - IRS](https://www.irs.gov/)
+            ## Descripción de las características más importantes:
+                        
+        - **Sexo** : Sexo del sujeto (Hombre,Mujer)
+        - **Edad** : Edad del sujeto (Rango 17-90)                
+        - **Rango de Edad** : Variable ordinal discretizada que divide a los sujetos en distintos tramos de edad
+        - **Horas Laborales (Semana)** : Estimación del número de horas trabajadas en una semana por el sujeto
+        - **Educación Superior** : Variable dicotómica que divide a los sujetos según si se han formado en educación superio o no (A partir de universidad)
+        - **Nivel Académico** : Nivel académico más alto cursado por el sujeto
+        - **Estado Civil** : Estado civil del sujeto (casado, soltero,divorciado...)
+        - **Trabajo** : Profesión a la que se dedica el sujeto   
             """)
+       
+
+
+   
+    with st.expander("Recordatorio: Tramos y Porcentajes de Impuestos en EE.UU."):
+        st.markdown("""
+        **Tramos federales de impuestos sobre la renta para personas individuales (2024):**
+
+        - Hasta $11,000 → 10%
+        - Rango (11,001 - $44,725) → 12%
+        - Rango (44,726 - $95,375) → 22%
+        - Rango (95,376 - $182,100) → 24%
+        - Rango (182,101 - $231,250) → 32%
+        - Rango (231,251 - $578,125) → 35%
+        - Más de $578,125 → 37%
+
+        Los impuestos en EE.UU. son progresivos. Es decir, cada tramo se grava con su tipo impositivo específico.
+        [Fuente oficial - IRS](https://www.irs.gov/)
+        """)
 
 elif menu == "Información relevante":
-
+    
     st.title("Información Legal y Fiscal Relevante")
     st.markdown("""
     Esta herramienta ha sido desarrollada con el objetivo de ayudar a las instituciones fiscales a identificar perfiles
     con riesgo fiscal potencial, basándose en patrones de ingreso y características sociodemográficas y laborales.
 
-    ###  Leyes y Reglamentos Relevantes:
+    ### Leyes y Reglamentos Relevantes:
     - **[Internal Revenue Code (IRC)](https://www.law.cornell.edu/uscode/text/26)**: Título 26 del Código de EE.UU.
     - **[FATCA (Foreign Account Tax Compliance Act)](https://www.irs.gov/businesses/corporations/foreign-account-tax-compliance-act-fatca)**: Ley de cumplimiento tributario de cuentas extranjeras.
     - **[Ley de Transparencia y Responsabilidad Fiscal](https://www.congress.gov/bill/113th-congress/house-bill/2531)**.
     - **[Formulario 1040 (Declaración de la renta individual)](https://www.irs.gov/forms-pubs/about-form-1040)**: incluye anexos relevantes según ingresos y ocupación.
     - **[Publication 505](https://www.irs.gov/forms-pubs/about-publication-505)**: Guía oficial sobre retenciones e impuestos estimados.
-
-    ###  Datos Analizados:
+                
+   ### Datos Analizados:
     - Nivel educativo
     - Tipo de ocupación
     - Sector de trabajo (público, privado, autónomo)
     - Horas trabajadas
     - Edad y estado civil
 
-    Este sistema no sustituye la auditoría ni decisiones legales. Es un **apoyo para análisis exploratorio** en fiscalización.
-    """)
+   Este sistema no sustituye la auditoría ni decisiones legales. Es un **apoyo para análisis exploratorio** en fiscalización.
+""")
 
 elif menu == "Ayuda":
-    st.title(" Centro de Ayuda")
-    st.markdown("""
-    Para obtener más ayuda o información sobre sus obligaciones fiscales:
+    st.title("Centro de Ayuda")
+    st.markdown("""Para obtener más ayuda o información sobre sus obligaciones fiscales:
+                🔗 [Sitio oficial del IRS - Individuals](https://www.irs.gov/individuals)
+                🔗 [Preguntas frecuentes - IRS](https://www.irs.gov/help/ita)
+                🔗 [Publicaciones fiscales en español](https://www.irs.gov/es/forms-pubs)
 
-    🔗 [Sitio oficial del IRS - Individuals](https://www.irs.gov/individuals)
-    🔗 [Preguntas frecuentes - IRS](https://www.irs.gov/help/ita)
-    🔗 [Publicaciones fiscales en español](https://www.irs.gov/es/forms-pubs)
-
-    También puede contactar con asesores fiscales certificados o consultar a su representante de Hacienda local.
-    """)
-
+También puede contactar con asesores fiscales certificados o consultar a su representante de Hacienda local.
+""")
 
 
---
+++ Data/app3.py
@@ -0,0 +1,33 @@
+descriptions = {
+                "Sexo": "Variable que define si el sujeto es Hombre o Mujer",
+                "Edad": "Edad del sujeto (Rango 17-90)",
+                "Rango de Edad": "Variable ordinal discretizada que divide a los sujetos en distintos tramos de edad",
+                "Horas Laborales (Semana)": "Estimación del número de horas trabajadas en una semana por el sujeto",
+                "Educación Superior": "Variable dicotómica que divide a los sujetos según si se han formado en educación superio o no (A partir de universidad) ",
+                "Nivel Académico": "Nivel académico más alto cursado por el sujeto",
+                "Estado Civil": "Estado civil del sujeto (casado, soltero,divorciado...)",
+                "Trabajo": "Profesión a la que se dedica el sujeto",
+                }
+
+            
+st.markdown('<div class="description-box">', unsafe_allow_html=True)
+st.markdown("### Descripción de las características más importantes:")
+st.markdown(descriptions)
+st.markdown('</div>', unsafe_allow_html=True)
+
+- Edad : Edad del sujeto (Rango 17-90)
+        - Rango de Edad : Variable ordinal discretizada que divide a los sujetos en distintos tramos de edad
+        - Horas Laborales (Semana) : Estimación del número de horas trabajadas en una semana por el sujeto
+        - Educación Superior : Variable dicotómica que divide a los sujetos según si se han formado en educación superio o no (A partir de universidad)
+        - Nivel Académico : Nivel académico más alto cursado por el sujeto
+        - Estado Civil : Estado civil del sujeto (casado, soltero,divorciado...)
+        - Trabajo : Profesión a la que se dedica el sujeto
+
+        -Sexo : Variable que define si el sujeto es Hombre o Mujer   
+            -Edad : Edad del sujeto (Rango 17-90)   
+            -Rango de Edad : Variable ordinal discretizada que divide a los sujetos en distintos tramos de edad   
+            -Horas Laborales (Semana) : Estimación del número de horas trabajadas en una semana por el sujeto   
+            -Educación Superior : Variable dicotómica que divide a los sujetos según si se han formado en educación superio o no (A partir de universidad)    
+            -Nivel Académico : Nivel académico más alto cursado por el sujeto   
+            -Estado Civil : Estado civil del sujeto (casado, soltero,divorciado...)   
+            -Trabajo : Profesión a la que se dedica el sujeto 
